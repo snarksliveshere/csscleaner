296e9c4670268f1e2dc17b326cce8bb5ea67ce9f
snarksliveshere <snarksliveshere@gmail.com>
2018-02-13 16:49:20 +0300

    add describe

--- csscleaner.php
+++ csscleaner.php
@@ -1,27 +1,58 @@
 <?php
-ini_set('max_execution_time', 900000);
-// TODO надо перебить это на генератор, чтобы память не ел
+ini_set('max_execution_time', 90000);
 class CSSConfig
 {
-    protected $urlWithoutHTML = true;
-    protected $customPath = __DIR__.'/assets/template/';
-    protected $jsFolder = 'js';
-    protected $cssFolder = 'css2';
-    protected $mainPageLink = 'http://demo4.ru/';
-    protected $resultCSS = 'result.css';
-    protected $counter = 0;
     /**
+     * @var bool
+     * есть ли .html у адреса
+     * have .html in url ?
+     */
+    protected $urlWithoutHTML = true;
+    /**
+     * @var string
+     * путь к папке, где лежат css файлы
+     * way to the folder with css files
+     */
+    protected $customPath = __DIR__.'/assets/template/';
+    /**
+     * @var string
+     * название папки, где лежат js скрипты
+     * name of the folder with js scripts
+     */
+    protected $jsFolder = 'js';
+    /**
+     * @var string
+     * название папки, где лежат css. Оптимально сделать копию папки, чтобы не чистить такие bootstrap.css & css, которые идут вместе с подключаемыми библиотеками
+     * name of the folder with css files. The good way is copy css folder without bootstrap.css and css from outer js libs
+     */
+    protected $cssFolder = 'css2';
+    /**
+     * @var string
+     * адрес главной страницы сайта
+     * main page url
+     */
+    protected $mainPageLink = 'http://demo4.ru/';
+    /**
+     * @var string
+     * название файла, которые получится на выходе
+     * name of the output file
+     */
+    protected $resultCSS = 'result.css';
+   /**
      * удалять ли комментарии
+     * is comments need to delete?
      * @var bool
      */
     protected $cleanComments = false;
     /**
-     * минимизируем в 1 строку
+     * минимизируем выходной css файл в 1 строку
+     * minify output css in one string
      * @var bool
      */
     protected $minifyAllInOneString = false;
     /**
-     * 1 класс 1 строка
+     * минимизируем выходной css по шаблону 1 класс 1 строка
+     * minify output css by pattern 1 class = 1 string
      * @var bool
      */
     protected $minifyOneClassOneString = false;
@@ -313,6 +344,7 @@ class CSSWalk
     extends CSSClassesFromPages
 {
     protected $lngCounter = 0;
+    protected $counter = 0;
     public function getCSS($result_class)
     {
         foreach (self::$cssClassesPath as $ki_css => $item) {
